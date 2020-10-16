2b0ab07ad12e4867dd5c7e7bae446cbfab6773f0
root <yassineaboub@live.fr>
2020-10-16 22:48:52 +0000

    10-prime.js

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,31 @@
+function isPrime(num){
+    if (num < 2){
+        return false;
+    }
+    for (let i = 2; i < num; i++){
+        if(num % i === 0){
+            return false
+        }
+    }
+    return true
+}
+
+function countPrimeNumbers() {
+    let n = 2
+    let j = 100;
+    let count = 0
+    while (n < j) {
+        if (isPrime(n)){
+            count++;
+        }
+    n ++;
+    }
+    
+    return count
+}
+const t0 = performance.now()
+for(let i = 0; i<100 ; i++){
+    countPrimeNumbers()
+}
+const t1 = performance.now()
+console.log(`Execution time of printing countPrimeNumbers was ${t1 - t0} milliseconds.`);
